b884b1281e11e3f62ef0d30e067c815f6badc698
mvvveen <maarten.vanveen@vanoord.com>
2017-03-03 20:19:57 +0100

    Update installanaconda.sh

--- installanaconda.sh
+++ installanaconda.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# This script installs and sets up Postgresql 9.6 with Postgis extension 2.3 fo$
+# This script installs Python 2.7.6 3.3.5 and Anaconda3
+# My Anaconda don't want none unless you got buns hun
 
-# Set variables
 
 clear
 echo "This script will install and configure anaconda software for your system"
+
 # Check whether you are root
 
 if [ "$(whoami)" == "root" ]; then
@@ -13,24 +14,29 @@ if [ "$(whoami)" == "root" ]; then
         exit 1
 fi
 
-
+# Let's download pyenv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 
+# Let's install some dependencies
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libpq-dev python-dev  pkg-config libpng-dev libfreetype6-dev
-#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-#echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-#echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
+# Let's set some of those environment variables and make sure them suckers stick after a reboot (needs some checks)
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
+# Here we install the actual python versions
 pyenv install 3.3.5
 pyenv rehash
 pyenv install 2.7.6
 pyenv rehash
 
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+# Let's install some demo stuff
 pyenv virtualenv 2.7.6 venv_2.7.6_deformdemo
 pyenv virtualenv 3.3.5 venv_3.3.5_deformdemo
 # Created venv in ~/.pyenv/versions/venv_3.3.5_deformdemo
@@ -46,27 +52,33 @@ pip install setuptools --upgrade
 pip install gunicorn
 python setup.py develop
 
-
+# Here we download that snake if it is not there already
 if [ ! -f  "./Anaconda3-4.3.0-Linux-x86_64.sh" ]; then
 wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
 fi
 
+# Let's start the sucker
 bash Anaconda3-4.3.0-Linux-x86_64.sh
 
+# More environment variables to set
 export PATH="~/anaconda3/bin:$PATH"
 echo export PATH="~/anaconda3/bin:$PATH" >> ~/.bashrc
 
+# Stuff used to make the websites work 
 conda install pandas -y
 conda install jupyter -y
-pip install pyjade
-pip install numpy
+conda/pip install pyjade
+conda/pip install numpy
 pip install geoalchemy2
 pip install sqlalchemy_utils
 pip install transaction
 pip install pyramid
 pip install geojson
 pip install gunicorn
+
+# This one is need to connect python to postgresql
 sudo apt-get build-dep -y python-psycopg2
+
+# This one is a conveniant way to start the website (look into changing this to systemctl services)
 sudo apt-get install -y supervisor
 sudo systemctl enable supervisor
-
